4db5691969e5464fb6a0176863c72ee0da5e42c5
alexander-w <work.1793593@gmail.com>
2020-11-04 12:49:22 +0600

    Up menu

--- components/UpMenu.js
+++ components/UpMenu.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
 export const UpMenu = () => {
+
+  const [menu, setMenu] = useState([
+    {
+      active: true
+    },
+    {
+      active: false
+    },
+    {
+      active: false
+    }
+  ])
+
   return (
     <View style={s.container}>
       <Text>
         Меню пользователя
       </Text> 
 
-      <View>
-        
+      <View style={s.menuWrapper}>
+        {
+          menu.map((item, index) => {
+            const active = item.active ? s.active : null
+            return (
+              <View key={index} style={[ s.menuItem,  active  ]}>
+              </View>
+            )   
+          })
+
+        }
+      
       </View>
     </View>
   )
@@ -18,5 +41,17 @@ export const UpMenu = () => {
 const s = new StyleSheet.create({
   container: {
     paddingHorizontal: 24
+  },
+  menuWrapper: {
+    flexDirection: 'row'
+  },
+  menuItem : {
+    width: 52,
+    height: 52,
+    borderRadius: 16,
+    backgroundColor: '#fff', 
+  },
+  active: {
+    flex: 1
   }
 })
