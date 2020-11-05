51d311cb4d899f6d3f2b11fda2c11b08f848a311
avorobyov <avorobyov@itl.agency>
2020-11-05 14:56:04 +0600

    Added icons, styles, main page updated. Screens to screens

--- components/UpMenu.js
+++ components/UpMenu.js
@@ -1,7 +1,48 @@
 import React, { useState } from 'react'
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
+import { FontAwesome5 } from '@expo/vector-icons'; 
+
 export const UpMenu = () => {
+
+  const [menu, setMenu] = useState([
+    {
+      id: 0,
+      title: 'Общая информация',
+      icon: 'user-edit',
+      active: true,
+      color: '125, 164, 255',
+      textColor: '#7098F5'
+    },
+    {
+      id: 1,
+      title: 'Задолженность',
+      icon: 'money-check',
+      active: false,
+      color: '253, 100, 100',
+      textColor: '#EC3C3C'
+    },
+    {
+      id: 2,
+      title: 'Нужно прочитать',
+      icon: 'check',
+      active: false,
+      color: '158, 109, 201',
+      textColor: '#925FBE'
+    }
+  ])
+
+  const changeCond = id => {
+    setMenu(
+      menu.map(item => {
+        if( item.id === id ) {
+          return {...item, active: true}
+        }
+        return {...item, active: false}
+      }) 
+    )
+  }
+
 
   return (
     <View style={s.container}>
@@ -10,14 +51,24 @@ export const UpMenu = () => {
       </Text>
 
       <View style={s.menuWrapper}>
-
-        <TouchableOpacity style={[s.menuItem, s.active]}>
-        </TouchableOpacity>
-        <TouchableOpacity style={[s.menuItem]}>
-        </TouchableOpacity>
-        <TouchableOpacity style={[s.menuItem]}>
-        </TouchableOpacity>
-
+      { 
+        menu.map((menuItem, i) => {
+          return (
+            <TouchableOpacity onPress={ () => {changeCond(menuItem.id)}} key={i} style={[s.menuItem, menuItem.active ?  {...s.active, backgroundColor: `rgba(${menuItem.color}, 0.4)`}  : s.disElem, menuItem.id === 2 ? {marginRight: 0} : null]}>
+              {
+                menuItem.active 
+                 ? (
+                   <View style={[ s.activeElem, ]}>
+                     <View style={{...s.iconBlock, backgroundColor: `rgba(${menuItem.color}, 1)`}}><Text> <FontAwesome5 name={menuItem.icon} size={16} color="white" /> </Text></View>
+                     <Text style={{fontSize: 10, color: menuItem.textColor}}>{ menuItem.title }</Text>
+                   </View> 
+                 )
+                 : <Text> <FontAwesome5 name={menuItem.icon} size={16} color="#9794A9" /> </Text>
+              }
+            </TouchableOpacity>
+          )
+        })
+      }
       </View>
     </View>
   )
@@ -35,6 +86,8 @@ const s = new StyleSheet.create({
     height: 52,
     borderRadius: 16,
     backgroundColor: '#fff',
+    marginRight: 16,
+    justifyContent: 'center',
   },
   active: {
     flex: 1
@@ -43,5 +96,23 @@ const s = new StyleSheet.create({
     fontSize: 24,
     color: '#333',
     marginVertical: 24
+  },
+  activeElem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  disElem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  iconBlock: {
+    width: 40,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginLeft: 6,
+    marginRight: 16
   }
 })
--- components/main/NavBar.js
+++ components/main/NavBar.js
@@ -1,11 +1,12 @@
 import React, { useContext, useState } from 'react'
 import { StyleSheet, View, Text, Button, TouchableOpacity } from 'react-native'
 import { AppContext } from '../../context/appContext'
+import { FontAwesome5 } from '@expo/vector-icons'
 
 export const NavBar = props => {
 
   const [activeTab, setActiveTab] = useState(1)
-  const { setScreen, tab } = useContext( AppContext )
+  const { setScreen, tab } = useContext(AppContext)
 
   const changeScreen = (param) => {
     setScreen(param)
@@ -13,22 +14,56 @@ export const NavBar = props => {
 
   return (
     <View style={s.navbar}>
-      <TouchableOpacity style={[s.btn, tab === 'Settings' ? s.active : null]} onPress={ () => { changeScreen('Settings', 1) } } >{tab != 'Settings' ?  <Text>Н-ки</Text> :  <Text style={{color: '#348D50'}}>Настройки</Text> }</TouchableOpacity>
-      <TouchableOpacity style={[s.btn, tab === 'StBook' ? s.active : null]} onPress={ () => { changeScreen('StBook', 2) } } >{tab != 'StBook' ?  <Text>З-ка</Text> :  <Text style={{color: '#348D50'}}>Зачетка</Text> }</TouchableOpacity>
-      <TouchableOpacity style={[s.btn, tab === null ? s.active : null, { marginRight: 0 }]} onPress={ () => { changeScreen(null, 3) } } >{tab != null ?  <Text>П-ль</Text> :  <Text style={{color: '#348D50'}}>Пользователь</Text> }</TouchableOpacity>
+      <TouchableOpacity style={[s.btn, tab === 'Settings' ? s.active : null]} onPress={() => { changeScreen('Settings', 1) }} >
+        {
+          tab != 'Settings' 
+            ? <Text><FontAwesome5 name="cogs" size={16} color="black" /></Text> 
+            : ( 
+              <View style={{flexDirection: 'row'}}>
+                <Text style={{marginRight: 16}}><FontAwesome5 name="cogs" size={16} color="#348D50" /></Text> 
+                <Text style={{ color: '#348D50' }}>Настройки</Text>
+              </View>
+            )
+            
+        }
+      </TouchableOpacity>
+      <TouchableOpacity style={[s.btn, tab === 'StBook' ? s.active : null]} onPress={() => { changeScreen('StBook', 2) }} >
+        {
+          tab != 'StBook' 
+            ? <Text><FontAwesome5 name="book" size={16} color="black" /></Text> 
+            : ( 
+              <View style={{flexDirection: 'row'}}>
+                <Text style={{marginRight: 16}}><FontAwesome5 name="book" size={16} color="#348D50" /></Text> 
+                <Text style={{ color: '#348D50' }}> Зачетка</Text>
+              </View>
+            )
+        }
+      </TouchableOpacity>
+      <TouchableOpacity style={[s.btn, tab === null ? s.active : null, { marginRight: 0 }]} onPress={() => { changeScreen(null, 3) }} >
+        {
+          tab != null
+            ? <Text> <FontAwesome5 name="users" size={16} color="black" /> </Text>
+            : ( 
+              <View style={{flexDirection: 'row'}}>
+                <Text style={{marginRight: 16}}><FontAwesome5 name="users" size={16} color="#348D50" /></Text> 
+                <Text style={{ color: '#348D50' }}> Пользователь</Text>
+              </View>
+            )
+        }
+      </TouchableOpacity>
     </View>
   )
 }
 
 const s = new StyleSheet.create({
-  
+
   navbar: {
     height: 80,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'space-between', 
+    justifyContent: 'space-between',
     backgroundColor: '#fff',
-    position: 'absolute', 
+    position: 'absolute',
     bottom: 0,
     width: '100%',
     paddingHorizontal: 24,
@@ -41,7 +76,7 @@ const s = new StyleSheet.create({
     width: 40,
     justifyContent: 'center',
     alignItems: 'center'
-    
+
   },
   active: {
     flex: 1,
